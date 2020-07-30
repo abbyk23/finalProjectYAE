8f2218694d6ba5236389ceab7131d397ad638949
Alexandra Collins <alexandracollins2022@gmail.com>
2020-07-30 14:03:26 -0500

    afternoon letter writer changes

--- finalProjectYAE/sixthViewController.swift
+++ finalProjectYAE/sixthViewController.swift
@@ -7,25 +7,68 @@
 //
 
 import UIKit
+import MessageUI
 
-class sixthViewController: UIViewController {
+class sixthViewController: UIViewController, MFMailComposeViewControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
 
     @IBOutlet weak var writeyourLetter: UILabel!
     
     @IBOutlet weak var backMainMenu: UIButton!
+    @IBOutlet weak var subject: UITextField!
+    @IBOutlet weak var body: UITextView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         backMainMenu.layer.cornerRadius = 10
-        // Do any additional setup after loading the view.
-        
-        
+        subject.delegate = self
+               body.delegate = self
         
     }
-    
-
     @IBAction func navSix(_ sender: Any) {
     }
     
+    @IBAction func sendEmail(_ sender: Any) {
+        let picker = MFMailComposeViewController()
+               picker.mailComposeDelegate = self
+                   
+               if let subjectText = subject.text {
+                   picker.setSubject(subjectText)
+               }
+               picker.setMessageBody(body.text, isHTML: true)
+                   
+               present(picker, animated: true, completion: nil)
+    }
+    
+
+// MFMailComposeViewControllerDelegate
+
+// 1
+func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+    dismiss(animated: true, completion: nil)
+}
+
+// UITextFieldDelegate
+    
+// 2
+func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+        
+    return true
+}
+    
+// UITextViewDelegate
+    
+// 3
+func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    body.text = textView.text
+        
+    if text == "\n" {
+        textView.resignFirstResponder()
+            
+        return false
+    }
+        
+    return true
+}
 }
