ae56112d141a6c1f21f6079564d4b20c0622b5ce
Abby Krier <abbykrier@gmail.com>
2020-07-30 17:56:13 -0500

    letter generator

--- finalProjectYAE/ViewController.swift
+++ finalProjectYAE/ViewController.swift
@@ -7,7 +7,74 @@
 //
 
 import UIKit
+import MessageUI
 
+class ifthViewController: UIViewController, MFMailComposeViewControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
+
+    @IBOutlet weak var letterGenerator: UILabel!
+    @IBOutlet weak var backOutlet: UIButton!
+    
+    @IBOutlet weak var heading: UITextField!
+    @IBOutlet weak var textbox: UITextView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backOutlet.layer.cornerRadius = 10
+        heading.delegate = self
+               textbox.delegate = self
+        textbox.text = "Dear state legislator,\nMy name is (insert name) and my community had been exposed to cancer-causing emissions of Ethylene Oxide for over 30 years. Please support HB 3888 to ban Ethylene Oxide emissions near schools.\nThank you,\n (insert name)"
+    }
+    
+
+    @IBAction func backtoMenu(_ sender: Any) {
+    }
+    
+    @IBAction func sendButton(_ sender: Any) {
+        let picker = MFMailComposeViewController()
+            picker.mailComposeDelegate = self
+                
+            if let subjectText = heading.text {
+                picker.setSubject(subjectText)
+            }
+            picker.setMessageBody(textbox.text, isHTML: true)
+                
+            present(picker, animated: true, completion: nil)
+        }
+        
+    
+    
+// MFMailComposeViewControllerDelegate
+
+// 1
+func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+    dismiss(animated: true, completion: nil)
+}
+
+// UITextFieldDelegate
+    
+// 2
+func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+        
+    return true
+}
+    
+// UITextViewDelegate
+    
+// 3
+func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    textbox.text = textView.text
+        
+    if text == "\n" {
+        textView.resignFirstResponder()
+            
+        return false
+    }
+        
+    return true
+}
+}
 class ViewController: UIViewController {
 
     @IBOutlet weak var welcomeLabel: UILabel!
